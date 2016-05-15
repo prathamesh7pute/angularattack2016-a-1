d5f13036739d903229ff4a6d000e21934afa067d
PunamSmahale <punam.mahale@gmail.com>
2016-05-15 00:33:36 -0400

    changes to home login template

--- src/app/+home/home.component.ts
+++ src/app/+home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MdCard } from '@angular2-material/card';
+import { MdButton } from '@angular2-material/button';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 
 import { AuthService } from '../shared';
 
@@ -8,7 +10,7 @@ import { AuthService } from '../shared';
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
-  directives: [MdCard]
+  directives: [MdCard, MdButton, MD_LIST_DIRECTIVES]
 })
 export class HomeComponent implements OnInit {
 
